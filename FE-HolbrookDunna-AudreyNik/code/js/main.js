7fe2f7ce84ce307f35b5251ad0f5f6581c22b515
nikdunna <nik.dunna@gmail.com>
2026-05-04 21:29:33 -0400

    Final boilerplate

--- FE-HolbrookDunna-AudreyNik/code/js/main.js
+++ FE-HolbrookDunna-AudreyNik/code/js/main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import "./style.css";
+import "../style.css";
 
 // ============================================================================
 // Renderer + DOM setup
